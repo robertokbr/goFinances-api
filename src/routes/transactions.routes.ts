c315930d9d5b4feae55dca94f03ae66378b9d91d
Roberto Junior <robertojuniordev@gmail.com>
2020-08-17 17:09:29 -0300

    Add import transaction by csv route

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -4,20 +4,26 @@ import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
 import Category from '../models/Category';
+import uploadConfig from '../config/upload';
+import multer from 'multer';
+import path from 'path';
+
+import ImportTransactionsService from '../services/ImportTransactionsService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
+const upload = multer(uploadConfig);
 
-// transactionsRouter.get('/', async (request, response) => {
-//   const transactionRepository = getCustomRepository(TransactionsRepository);
-//   const balance = await transactionRepository.getBalance();
-//   const categoryRepository = getRepository(Category);
-//   const transactions = await transactionRepository.find({
-//     join: { Category },
-//     where: { category_id, id },
-//     select: {},
-//   });
-// });
+transactionsRouter.get('/', async (request, response) => {
+  const transactionRepository = getCustomRepository(TransactionsRepository);
+  const categoryRepository = getRepository(Category);
+
+  const balance = await transactionRepository.getBalance();
+  const transactionsWithoutBalance = await transactionRepository.find();
+  // const transaction = transactionsWithoutBalance.map( transaction =>)
+
+
+});
 
 transactionsRouter.post('/', async (request, response) => {
   const { title, value, type, category } = request.body;
@@ -36,11 +42,20 @@ transactionsRouter.delete('/:id', async (request, response) => {
   const { id } = request.params;
   const deletReturn = new DeleteTransactionService();
   await deletReturn.execute(id);
+
+
+
+  
   return response.status(204);
 });
 
-// transactionsRouter.post('/import', async (request, response) => {
-//   // TODO
-// });
+transactionsRouter.post('/import',upload.single('file') ,
+ async (request, response) => {
+const adress = path.resolve(__dirname, '..','..', 'tmp','4e32df26139d9927ea-logo.pnga-logo.png')
+const importTransactions = new ImportTransactionsService();
+
+const transactions = importTransactions.execute(request.file.path);
+
+});
 
 export default transactionsRouter;
