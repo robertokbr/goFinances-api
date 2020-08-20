3b3a8e89a9a7709cc617ae86d35b37720f0162d9
Roberto Junior <robertojuniordev@gmail.com>
2020-08-19 23:29:07 -0300

    Alter Relation Transaction/Category

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -26,7 +26,7 @@ class Transaction {
   @Column()
   category_id: string;
   
-  @ManyToOne(() => Category, { eager: true })
+  @ManyToOne(() => Category, category => category.transaction,  { eager: true })
   @JoinColumn({ name: 'category_id' })
   category: Category;
 
